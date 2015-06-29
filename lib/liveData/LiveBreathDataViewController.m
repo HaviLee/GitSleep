54193cf6134c87ea59bd08152d08aba334f09e00
Havi_li <1058695900@qq.com>
2015-06-29 16:23:15 +0800

    build 6.29

--- lib/liveData/LiveBreathDataViewController.m
+++ lib/liveData/LiveBreathDataViewController.m
@@ -213,28 +213,32 @@
         if ([[resposeDic objectForKey:@"ReturnCode"]intValue]==200){
             if ([[[[resposeDic objectForKey:@"SensorData"] objectAtIndex:0] objectForKey:@"Data"] count]>0) {
                 NSArray *arr1 = [[[resposeDic objectForKey:@"SensorData"] objectAtIndex:0] objectForKey:@"Data"];
+                //                int j = 0;
                 NSArray *arr = [arr1 sortedArrayUsingComparator:^NSComparisonResult(NSDictionary *p1, NSDictionary *p2){
                     return [[p1 objectForKey:@"At"] compare:[p2 objectForKey:@"At"]];
                     
                 }];
-                HaviLog(@"测试呼吸数组%@和时间%@",arr,self.queryEndDateString);
-//                NSString *dateString = [[arr lastObject]objectForKey:@"At"];
-//                NSDate *date = [self.dateFormmatterHeart dateFromString:dateString];
-//                NSDate *newDate = [[NSDate date]dateByAddingTimeInterval:8*60*60];
-//                if ([newDate timeIntervalSinceDate:date]>30) {
-//                    for (int i =(int)self.ceshiArr.count-1; i>0; i--) {
-//                        self.ceshiArr[i] = self.ceshiArr[i-1];
-//                    }
-//                    [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:4]];
-//                }else{
-//                }
+                HaviLog(@"测试心率数组%@和时间%@",arr,self.queryEndDateString);
+                //                NSString *dateString = [[arr lastObject]objectForKey:@"At"];
+                //                NSDate *date = [self.dateFormmatterHeart dateFromString:dateString];
+                //                NSDate *newDate = [[NSDate date]dateByAddingTimeInterval:8*60*60];
+                //                if ([newDate timeIntervalSinceDate:date]>30) {
+                //                    for (int i =(int)self.ceshiArr.count-1; i>0; i--) {
+                //                        self.ceshiArr[i] = self.ceshiArr[i-1];
+                //                    }
+                //                    [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:39]];
+                //                }else{
+                //                }
+                //
                 for (int i=0; i<arr.count; i++) {
                     if (self.queryEndDateString) {
                         for (int i =(int)self.ceshiArr.count-1; i>0; i--) {
                             self.ceshiArr[i] = self.ceshiArr[i-1];
                         }
                         [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:[[[arr objectAtIndex:i] objectForKey:@"Value"] intValue]]];
+                        
                         /*
+                         NSString *dateString = [[arr objectAtIndex:i]objectForKey:@"At"];
                          NSDate *date = [self.dateFormmatterHeart dateFromString:dateString];
                          if ([date timeIntervalSinceDate:self.queryEndDateString]>0) {
                          for (int i =(int)self.ceshiArr.count-1; i>0; i--) {
@@ -242,29 +246,33 @@
                          }
                          [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:[[[arr objectAtIndex:i] objectForKey:@"Value"] intValue]]];
                          }else{
+                         
                          }
                          */
                     }else {
+                        //第一次请求
                         if (i<230) {
                             [self.ceshiArr replaceObjectAtIndex:i withObject:[NSNumber numberWithInt:[[[arr objectAtIndex:i] objectForKey:@"Value"]intValue]]];
                         }
-                        
                     }
                 }
+                
                 NSString *s = [[arr lastObject] objectForKey:@"At"];
                 self.queryEndDateString = [self.dateFormmatterHeart dateFromString:s];
-                
             }else{
+                //是200但是没有数据
                 for (int i =(int)self.ceshiArr.count-1; i>0; i--) {
                     self.ceshiArr[i] = self.ceshiArr[i-1];
                 }
-                [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:4]];
+                [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:39]];
             }
+            
         }else{
+            //这个是returncode 不是200
             for (int i =(int)self.ceshiArr.count-1; i>0; i--) {
                 self.ceshiArr[i] = self.ceshiArr[i-1];
             }
-            [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:4]];
+            [self.ceshiArr replaceObjectAtIndex:0 withObject:[NSNumber numberWithInt:39]];
         }
         self.dataSource = self.ceshiArr;
     } failure:^(YTKBaseRequest *request) {
@@ -322,7 +330,7 @@
         y = padding + (padding/5)*(value-10);
     }else if ((value>15 || value == 15) && value <20){
         y = 2*padding + (padding/5)*(value-15);
-    }else if (value>20){
+    }else if (value>20||value==20){
         y = 3*padding;
     }else if (y<5){
         y = 0;
