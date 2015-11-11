5fdb7cdb0e01b086f8d52e95bc555efb61d80b26
Havi_li <1058695900@qq.com>
2015-11-11 16:04:44 +0800

    build master 11.11为我的设备列表添加了下拉刷新功能

--- controller/MyDeviceListViewController.m
+++ controller/MyDeviceListViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "MyDeviceListViewController.h"
+#import "ODRefreshControl.h"
 
-@interface MyDeviceListViewController ()
+@interface MyDeviceListViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property (nonatomic, strong) UITableView *myDeviceListView;
+@property (nonatomic, strong) ODRefreshControl *refreshControl;
+
 
 @end
 
@@ -19,7 +24,65 @@
     // Do any additional setup after loading the view.
     self.bgImageView.image = [UIImage imageNamed:@""];
     self.view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.myDeviceListView];
+    _refreshControl = [[ODRefreshControl alloc] initInScrollView:_myDeviceListView];
+    [_refreshControl addTarget:self action:@selector(refresh) forControlEvents:UIControlEventValueChanged];
+}
+//刷新
+- (void)refresh{
+    __weak typeof(self) weakSelf = self;
     
+    HaviLog(@"刷新ok");
+    [weakSelf.refreshControl endRefreshing];
+    
+}
+
+#pragma setter meathod
+
+- (UITableView *)myDeviceListView
+{
+    if (!_myDeviceListView) {
+        _myDeviceListView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height-64) style:UITableViewStyleGrouped];
+        _myDeviceListView.dataSource = self;
+        _myDeviceListView.delegate = self;
+        _myDeviceListView.backgroundColor = [UIColor lightGrayColor];
+        
+        
+    }
+    return _myDeviceListView;
+}
+
+#pragma tableview delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 30;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIndentifier = @"cellIndentifier";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIndentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIndentifier];
+    }
+    cell.textLabel.text = @"li";
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 0.001;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.001;
 }
 
 - (void)didReceiveMemoryWarning {
